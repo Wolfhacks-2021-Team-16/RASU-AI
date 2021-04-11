cb7c92339f2dfd4fe1a7c3ea760638695fe030f4
AizazL <aizazahmedlodhi@gmail.com>
2021-04-11 13:49:15 -0400

    dashboard

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require("./routes/login");
 var registerRouter = require('./routes/register');
+var dashboardRouter = require('./routes/dashboard');
+
 
 var app = express();
 
@@ -33,6 +35,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter)
 app.use('/register', registerRouter)
+app.use('/dashboard', dashboardRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
